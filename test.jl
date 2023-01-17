df1d41a03b0d5102164cc098d5f3d4590d31f0f3
Cruz Godar <cruz.godar@gmail.com>
2023-01-17 15:18:50 -0800

    Julia recursion test

--- test.jl
+++ test.jl
@@ -1,17 +1,9 @@
-# Returns a brightness for the complex number z in the Mandelbrot set. Optionally allows c to be set to compute the brightness for a Julia set instead.
-function mandelbrotbrightness(z; c = z, maxiterations = 100)
-    brightness = 0
-    
-    current_z = z
-    
-    for iteration = 1:maxiterations
-        current_z = current_z^2 + c
-        
-        if abs(current_z) >= 2
-            brightness = 0
-            break
-        end
+function factorial(n::{<:Integer})
+    if n == 0
+        return 1
     end
     
-    return brightness
+    return factorial(n - 1)
 end
+
+println(factorial(5))
