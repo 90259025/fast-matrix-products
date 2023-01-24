ce6c4041ebeaffee72d4b8e76f733545857679cc
Andy Huchala <ahuchala@gmail.com>
2023-01-23 18:48:36 -0800

    Middle Product alive and well (called MP not RMP now)

--- functions.jl
+++ functions.jl
@@ -9,21 +9,24 @@ end
 factorial(n::T where {T <: Integer}) = factorial(convert(BigInt, n))
 
 
-# IN: [a_1,...,a_2n], [b_1,...,b_n]
+
+# Karastuba style, from https://hal.inria.fr/inria-00071921/document
+
+# IN: [y_1,...,y_n], [z_1,...,z_2n-1]
 # OUT: [c_n-1,..., c_2n-1] where c_k = \sum_{i+j=j}a_i b_j
-function RMP(A::Vector{<: Union{Integer, Rational{<: Integer}}}, B::Vector{<: Union{Integer, Rational{<: Integer}}})
-    n = length(A) >> 1  # n = len(A)/2
-    n2 = n >> 1         # n2 = n/2
-    
+function MP(y::Vector{<: Union{Integer, Rational{<: Integer}}}, z::Vector{<: Union{Integer, Rational{<: Integer}}})
+    n = length(y)
     if n == 1
-        return [A[1] * B[1]]
-    elseif n2*2 != n    # n is odd
-        return 1        #TODO
+        return [y[1] * z[1]]
     end
-
-    α = RMP(A[1:n] .+ A[(n2 + 1):(3*n2)], B[(n2 + 1):end])
-    β = RMP(A[(n2 + 1):(3*n2)], B[1:(n2)] .- B[(n2 + 1):end])
-    γ = RMP(A[(n2 + 1):(3*n2)] .+ A[(n + 1):2*n], B[1:(n2)])
-
-    return [α + β; γ - β]
+    n0 = n >> 1
+    n1 = (n+1) >> 1
+    α = MP(y[n0+1:end], z[1:2*n1-1] .+ z[n1+1:3*n1-1])
+    if iseven(n)
+        β = MP(y[n1+1:n] .- y[1:n0], z[n1+1:3*n1-1])
+    else
+        β = MP([y[n0+1]; y[n1+1:n] .- y[1:n0]],z[n1+1:3*n1-1])
+    end
+    γ = MP(y[1:n0], z[n1+1:n1+2*n0 - 1] .+ z[2*n1+1:2*n-1])
+    return [α[1:n1] .- β[1:n1]; γ[1:n0] .+ β[1:n0]]
 end
